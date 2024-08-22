7cd615a08d913b14053d8c81eaf6ecfa0fa77d51
Chamika Vishmal <chamika.vishmal@xeneta.com>
2024-08-22 22:38:28 +0200

    test

---
+++ main.tf
@@ -0,0 +1,12 @@
+# test
+resource "aws_route53_record" "test" {
+  zone_id = "test"
+  name    = "test101.abc.com"
+  type    = "A"
+
+  alias {
+    name                   = "abcsds.com"
+    zone_id                = "aws_lb.redirector.zone_id"
+    evaluate_target_health = false
+  }
+}
